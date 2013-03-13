d9295c7dcfe1847d5bd02e5a5a5878c7e7227e4d
Ken Ohata <ohata@undapt.jp>
2013-03-13 17:08:34 +0900

    api/users/sign_out

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -19,17 +19,12 @@ class Api::SessionsController < Devise::SessionsController
   end
 
   def destroy
-    redirect_path = after_sign_out_path_for(resource_name)
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
-
-    # We actually need to hardcode this as Rails default responder doesn't
-    # support returning empty response on GET request
-    respond_to do |format|
-      format.any(*navigational_formats) { redirect_to redirect_path }
-      format.all do
-        head :no_content
-      end
+    begin
+      @user = User.find_by_authentication_token(params[:auth_token])
+      @user.reset_authentication_token!
+      render json: { messages: ['reset token'] }
+    rescue
+      render json: { errors: ['invalid auth token'] }
     end
   end
 end
